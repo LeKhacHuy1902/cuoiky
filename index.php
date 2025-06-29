4981f9d768049faf178bae374230548c767425b5
VinhAn-dev <anpro476@gmail.com>
2025-06-29 20:26:20 +0700

    demo giao diện xem ở index tạo liên kết người 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fastsickduck</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <ul>
+        <li><a href="views/home.php">trang home</a></li>
+        <li><a href="views/login.php">trang login</a></li>
+        <li><a href="views/register.php">trang register</a></li>
+        <li><a href="views/booking.php">trang booking</a></li>
+        <li><a href="views/orders.php">trang orders</a></li>
+        <li><a href="views/admin/dashboard.php">trang dashboard</a></li>
+        <li><a href="views/admin/services.php">trang services</a></li>
+        <li><a href="views/admin/users.php">trang user</a></li>
+    </ul>
+</body>
+</html>
